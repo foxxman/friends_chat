5cb3dcf3b41ca38d8476bcaec5da24ff705c0960
Максим Чпров <chuprov9977@gmail.com>
2023-03-10 14:54:22 +0300

    ...

--- server/build/models/Token.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-const schema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: "User" },
-    refreshToken: { type: String, required: true },
-}, {
-    timestamps: true,
-});
-export default model("Token", schema);
-//# sourceMappingURL=Token.js.map
--- server/build/utils/strToObjId.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-const strToObjId = (str) => new mongoose.Types.ObjectId(str);
-export default strToObjId;
-//# sourceMappingURL=strToObjId.js.map
